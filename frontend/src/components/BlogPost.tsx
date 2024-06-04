65f5479306a17ec3b5087bd08d7771ffa5cf9998
Prasad-D-Ware <prasadware42116@gmail.com>
2024-06-04 16:24:48 +0530

    basic view fix

--- frontend/src/components/BlogPost.tsx
+++ frontend/src/components/BlogPost.tsx
@@ -7,13 +7,13 @@ export const BlogPost = ({ blog }: { blog: Blog }) => {
     <div>
       <Appbar />
       <div className="flex justify-center">
-        <div className="grid grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
-          <div className="col-span-8">
+        <div className="grid grid-cols-1 lg:grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
+          <div className="lg:col-span-8">
             <div className="text-5xl font-extrabold">{blog.title}</div>
             <div className="text-slate-500 pt-2">Post on 2nd December 2023</div>
             <div className="pt-4">{blog.content}</div>
           </div>
-          <div className="col-span-4">
+          <div className="lg:col-span-4">
             <div className="text-slate-600 text-lg">Author</div>
             <div className="flex w-full pt-2">
               <div className="pr-4 flex flex-col justify-center">

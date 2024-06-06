594cb97b688239af768e1fe23e96b630f4c4f50e
Prasad-D-Ware <prasadware42116@gmail.com>
2024-06-06 23:26:47 +0530

    made a basic home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,36 @@
+import { useNavigate } from "react-router-dom";
+import { Quote } from "../components/Quote";
+
 export const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2">
+      <Quote quote="BlogHub : One stop for your Blogging endeavours" />
+      <div className="flex justify-center items-center">
+        <div>
+          <div className="text-3xl font-extrabold p-3 flex justify-center">
+            BlogHub
+          </div>
+          <button
+            onClick={() => {
+              navigate("/signup");
+            }}
+            type="button"
+            className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 "
+          >
+            Sign Up
+          </button>
+          <button
+            onClick={() => {
+              navigate("/signin");
+            }}
+            type="button"
+            className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 "
+          >
+            Sign In
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };

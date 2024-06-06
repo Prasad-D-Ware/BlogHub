614d6ed67c12083c6b2806414d882f78d00912c9
Prasad-D-Ware <prasadware42116@gmail.com>
2024-06-06 23:20:18 +0530

    modified quote component

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,12 +1,9 @@
-export const Quote = () => {
+export const Quote = ({ quote }: { quote: string }) => {
   return (
     <div className="h-screen bg-slate-200 flex justify-center flex-col ">
       <div className="flex justify-center">
         <div className="max-w-lg ">
-          <div className="text-3xl font-bold">
-            "Let your inner writer come out. Post the blogs about the fields you
-            are interested in.."
-          </div>
+          <div className="text-3xl font-bold">{quote}</div>
           <div className="max-w-md text-xl font-semibold mt-3">Prasad Ware</div>
           <div className="max-w-md  text-sm font-light text-slate-400">
             Developer | BlogHub

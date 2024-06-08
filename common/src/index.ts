c77156c0dd56e5698856271dc36a9fbcfe1025f2
Prasad-D-Ware <prasadware42116@gmail.com>
2024-06-08 23:41:33 +0530

    doc:added documentation

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,27 +1,32 @@
 import z from "zod";
 
+//signup input validations for backend
 export const signupInput = z.object({
   email: z.string().email(),
   password: z.string().min(6),
   name: z.string().optional(),
 });
 
+//signin input validations for backend
 export const signinInput = z.object({
   email: z.string().email(),
   password: z.string().min(6),
 });
 
+//creating input validations for backend
 export const creatBlogInput = z.object({
   title: z.string(),
   content: z.string(),
 });
 
+//updating input validations for backend
 export const updateBlogInput = z.object({
   title: z.string(),
   content: z.string(),
   id: z.number(),
 });
 
+//types for frontend
 export type SignupInput = z.infer<typeof signupInput>;
 export type SigninInput = z.infer<typeof signinInput>;
 export type CreatBlogInput = z.infer<typeof creatBlogInput>;
